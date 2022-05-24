fbec828af6b09db4e57f993613eb41b389f17609
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-24 20:00:28 +0200

    Agregamos /api

--- ud26_2_team/src/main/java/com/mska/ejercicio/controllers/ProyectoController.java
+++ ud26_2_team/src/main/java/com/mska/ejercicio/controllers/ProyectoController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mska.ejercicio.dto.Proyecto;
 import com.mska.ejercicio.service.ProyectoServiceImp;
 
-
+@RestController
+@RequestMapping("/api")
 public class ProyectoController {
 	@Autowired
 	ProyectoServiceImp proyectoServiceImp;
