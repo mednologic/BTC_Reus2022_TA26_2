3ede57495d783911365d262d27c69faa58503019
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-24 19:39:04 +0200

    Areglo nombre tablas

--- ud26_2_team/src/main/java/com/mska/ejercicio/dto/Cientifico.java
+++ ud26_2_team/src/main/java/com/mska/ejercicio/dto/Cientifico.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "cientificos")
+@Table(name = "cientifico")
 public class Cientifico {
 
 	// atributos
