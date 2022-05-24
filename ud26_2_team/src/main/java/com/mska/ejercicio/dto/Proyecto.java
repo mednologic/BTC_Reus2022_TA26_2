227507883daa4631b0641d831309944bd875cfe8
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-24 19:39:35 +0200

    Arreglo nombre tabla 2

--- ud26_2_team/src/main/java/com/mska/ejercicio/dto/Proyecto.java
+++ ud26_2_team/src/main/java/com/mska/ejercicio/dto/Proyecto.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "proyectos")
+@Table(name = "proyecto")
 public class Proyecto {
 
 	// atributos
